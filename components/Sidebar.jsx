3680c77d9285bcad35ffecfffbf430f25a13480a
blessedmadukoma <blessedmadukoma@gmail.com>
2023-02-06 11:45:22 +0100

    created sidebar component, wrapped everything in _app.js using the Sidebar component so it stays on all the pages

---
+++ components/Sidebar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { RxSketchLogo, RxDashboard, RxPerson } from "react-icons/rx";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import { FiSettings } from "react-icons/fi";
+
+const Sidebar = ({ children }) => {
+  return (
+    <section>
+      <aside className="flex">
+        <div className="fixed w-20 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between">
+          <section className="flex flex-col items-center">
+            <Link href="/">
+              <div className="bg-purple-800 text-white p-3 rounded-lg inline-block">
+                <RxSketchLogo />
+              </div>
+            </Link>
+
+            <span className="border-b-[1px] border-gray-200 w-full p-2"></span>
+
+            <Link href="/">
+              <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer m-4 p-3 rounded-lg inline-block">
+                <RxDashboard />
+              </div>
+            </Link>
+
+            <Link href="/person">
+              <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer m-4 p-3 rounded-lg inline-block">
+                <RxPerson />
+              </div>
+            </Link>
+
+            <Link href="/customers">
+              <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer m-4 p-3 rounded-lg inline-block">
+                <HiOutlineShoppingBag />
+              </div>
+            </Link>
+            <Link href="/settings">
+              <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer m-4 p-3 rounded-lg inline-block">
+                <FiSettings />
+              </div>
+            </Link>
+          </section>
+        </div>
+      </aside>
+      <main className="ml-20 w-full">{children}</main>
+    </section>
+  );
+};
+
+export default Sidebar;
+
+// wrapped everything in _app.js using the Sidebar component so it stays on all the pages
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 
-import Header from "./components/Header";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
